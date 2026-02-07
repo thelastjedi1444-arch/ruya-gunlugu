a29faf03888182ec8ca6d48961fed20f772bb3da
Ahmet Vural <ahmetvural@192.168.1.123>
2026-02-08 02:37:55 +0300

    Correct PrismaClient datasources configuration for new Prisma version

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -2,6 +2,16 @@ import { PrismaClient } from '@prisma/client'
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient }
 
-export const prisma = globalForPrisma.prisma || new PrismaClient()
+const prismaClientSingleton = () => {
+    return new PrismaClient({
+        datasources: {
+            db: {
+                url: process.env.DATABASE_URL
+            }
+        }
+    })
+}
+
+export const prisma = globalForPrisma.prisma || prismaClientSingleton()
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
