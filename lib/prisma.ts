8b9e0282e12e719b6eaef63674fdcf12212eef91
Ahmet Vural <ahmetvural@192.168.1.123>
2026-02-08 11:40:18 +0300

    Use LibSQL adapter for Prisma 7 PrismaClient

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,9 +1,16 @@
 import { PrismaClient } from '@prisma/client'
+import { PrismaLibSQL } from '@prisma/adapter-libsql'
+import { createClient } from '@libsql/client'
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient }
 
 const prismaClientSingleton = () => {
-    return new PrismaClient()
+    const url = process.env.DATABASE_URL || 'file:./dev.db'
+
+    const libsql = createClient({ url })
+    const adapter = new PrismaLibSQL(libsql)
+
+    return new PrismaClient({ adapter })
 }
 
 export const prisma = globalForPrisma.prisma || prismaClientSingleton()
